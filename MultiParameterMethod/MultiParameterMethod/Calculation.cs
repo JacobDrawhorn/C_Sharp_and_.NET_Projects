9407ef5f0790d12d7209ae4f9af220c3b8ba9448
JacobDrawhorn <87413802+JacobDrawhorn@users.noreply.github.com>
2021-11-05 22:06:15 -0700

    Add files via upload

---
+++ MultiParameterMethod/MultiParameterMethod/Calculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiParameterMethod
+{
+    class Calculation
+    {
+        public int Message(int userNum1, int userNum2 = 0)
+        {
+            int result = (userNum1 - userNum2) / 2;
+                return result;
+        }
+    }
+}
