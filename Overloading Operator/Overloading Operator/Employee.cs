5bc02604679c4f84414364daf3885a0ab4acf6a3
JacobDrawhorn <87413802+JacobDrawhorn@users.noreply.github.com>
2021-11-05 19:28:04 -0700

    Add files via upload

---
+++ Overloading Operator/Overloading Operator/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Operator
+{
+    class Employee
+    {
+        public int Id { get; set; }//decalred property must be inside the class Employee
+
+        public static bool operator ==(Employee person1, Employee person2) //overloading operator, checks if person1 id is equal to person2 id
+        {
+            if (person1==person2)
+            {
+                return true;  //1 = 2 false
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee person1, Employee person2)
+        {
+            if (person1!=person2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Overloading Operator/Overloading Operator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Employee person1 = new Employee //create an instance of a class and then call the method
+            {
+                Id = 1 //object.property
+            };  
+
+            Employee person2 = new Employee
+            {
+                Id = 2
+            };
+
+            //Console.WriteLine(person1!=person2);
+            Console.WriteLine(person1==person2); //prints out result 
+            Console.ReadLine();
+        }
+    }
+}
