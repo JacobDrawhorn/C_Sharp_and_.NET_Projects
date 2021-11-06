570b15f0ef526b7e804e03ab19a0386012d5bf3d
JacobDrawhorn <87413802+JacobDrawhorn@users.noreply.github.com>
2021-11-05 22:48:19 -0700

    Add files via upload

---
+++ LogAssignment/LogAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number:");
+            //string userNum = Console.ReadLine();
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            DateTime timeFromNow = DateTime.Now.AddHours(userNum);
+            Console.WriteLine(timeFromNow);
+
+            //File.WriteAllText(@"C:\Users\CarlyScanlan\Desktop\number.txt", userNum);
+            //string txtFile = File.ReadAllText(@"C:\Users\CarlyScanlan\Desktop\number.txt");
+            //Console.WriteLine(txtFile);
+            Console.Read();
+
+        }
+    }
+}
