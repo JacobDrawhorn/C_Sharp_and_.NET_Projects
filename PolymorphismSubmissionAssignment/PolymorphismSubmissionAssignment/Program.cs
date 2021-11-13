0ff8c1b5668309168546e1231f73a347d5597549
JacobDrawhorn <87413802+JacobDrawhorn@users.noreply.github.com>
2021-11-12 20:03:18 -0800

    Add files via upload

---
+++ PolymorphismSubmissionAssignment/PolymorphismSubmissionAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmissionAssignment
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            //instantiate class employee passing in the properties firstName and lastName
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student"};
+            IQuittable insertnamehere = new Employee();
+
+            //call to the method SayName
+            employee.SayName();
+
+
+            //Call to the method Quit            
+            insertnamehere.Quit(employee);
+            Console.ReadLine();
+        }
+        
+    }
+}
